8157076c2be2ab11d7e5ff869876f10c83b382c7
emr1195 <73007113+emr1195@users.noreply.github.com>
2023-11-28 13:37:07 -0500

    .

--- src/components/organisms/news/News.tsx
+++ src/components/organisms/news/News.tsx
@@ -1,68 +1,89 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { BsChevronCompactLeft, BsChevronCompactRight } from "react-icons/bs";
-import { RxDotFilled } from "react-icons/rx";
 
 function Slider() {
-  const slides = [
-    {
-      url: "./src/assets/img/s-1.jpg",
-    },
-    {
-      url: "./src/assets/img/s-2.jpg",
-    },
-    {
-      url: "./src/assets/img/s-3.png",
-    },
-  ];
+    const slides = [{
+            url: "./src/assets/img/s-1.jpg",
+        },
+        {
+            url: "./src/assets/img/s-2.jpg",
+        },
+        {
+            url: "./src/assets/img/s-3.png",
+        },
+    ];
 
-  const [currentIndex, setCurrentIndex] = useState(0);
+    const [currentIndex, setCurrentIndex] = useState(0);
 
-  const prevSlide = () => {
-    const isFirstSlide = currentIndex === 0;
-    const newIndex = isFirstSlide ? slides.length - 1 : currentIndex - 1;
-    setCurrentIndex(newIndex);
-  };
 
-  const nextSlide = () => {
-    const isLastSlide = currentIndex === slides.length - 1;
-    const newIndex = isLastSlide ? 0 : currentIndex + 1;
-    setCurrentIndex(newIndex);
-  };
+    const nextSlide = () => {
+        const newIndex = (currentIndex + 1) % slides.length;
+        setCurrentIndex(newIndex);
+    };
 
-  const goToSlide = (slideIndex) => {
-    setCurrentIndex(slideIndex);
-  };
 
-  return (
-    <div className=" w-full mx-auto  bg-white p-4">
-      {/* <div className="max-w-[1400px] h-[500px] w-full m-auto py-1 px-1 relative group"> */}
-      <div className=" h-[500px] w-full m-auto py-1 px-1 relative group">
-        <div
-          style={{ backgroundImage: `url(${slides[currentIndex].url})` }}
-          className="w-full h-full rounded-md bg-center bg-cover duration-500"
-        ></div>
+    const prevSlide = () => {
+        const newIndex = currentIndex === 0 ? slides.length - 1 : currentIndex - 1;
+        setCurrentIndex(newIndex);
+    };
 
-        <div className="hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] left-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer">
-          <BsChevronCompactLeft onClick={prevSlide} size={30} />
-        </div>
 
-        <div className="hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] right-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer">
-          <BsChevronCompactRight onClick={nextSlide} size={30} />
-        </div>
-        <div className="flex top-4 justify-center py-2">
-          {slides.map((slide, slideIndex) => (
-            <div
-              key={slideIndex}
-              onClick={() => goToSlide(slideIndex)}
-              className="text-2xl cursor-pointer"
-            >
-              <RxDotFilled />
-            </div>
-          ))}
-        </div>
-      </div>
-    </div>
-  );
+    const goToSlide = (slideIndex) => {
+        setCurrentIndex(slideIndex);
+    };
+
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            nextSlide();
+        }, 5000);
+
+
+        return () => clearInterval(interval);
+    }, [currentIndex]);
+
+    return ( <
+        div className = "w-full mx-auto bg-white p-4" >
+        <
+        div className = "h-[500px] w-full m-auto py-1 px-1 relative group" >
+        <
+        div style = {
+            { backgroundImage: `url(${slides[currentIndex].url})` } }
+        className = "w-full h-full rounded-md bg-center bg-cover duration-500" >
+        < /div>
+
+        <
+        div className = "hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] left-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer" >
+        <
+        BsChevronCompactLeft onClick = { prevSlide }
+        size = { 30 }
+        /> <
+        /div>
+
+        <
+        div className = "hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] right-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer" >
+        <
+        BsChevronCompactRight onClick = { nextSlide }
+        size = { 30 }
+        /> <
+        /div> <
+        div className = "flex top-4 justify-center py-2" > {
+            slides.map((slide, slideIndex) => ( <
+                div key = { slideIndex }
+                onClick = {
+                    () => goToSlide(slideIndex) }
+                className = { `text-2xl cursor-pointer ${
+                slideIndex === currentIndex ? "text-blue-500" : ""
+              }` } >
+
+                <
+                /div>
+            ))
+        } <
+        /div> <
+        /div> <
+        /div>
+    );
 }
 
 export default Slider;
--- src/components/organisms/org/Org.tsx
+++ src/components/organisms/org/Org.tsx
@@ -1,168 +1,171 @@
-import React from "react";
+const coorNac = [
+  {
+    name: "Arturo Martez",
+    role: "Coordinador Nacional",
+    image: "./src/assets/img/arturo.png",
+  },
+  
+];
+
+const comNac = [
+  {
+    name: "Urbano Caceres",
+    role: "Coordinador de Adiestramiento",
+    image: "./src/assets/img/urbano.png",
+  },
+  {
+    name: "Luis Lopez",
+    role: "Coordinador de Alcance",
+    image: "./src/assets/img/luis.png",
+  },
+  {
+    name: "Rafael Romero",
+    role: "Coordinador de Programa",
+    image: "./src/assets/img/rafael.png",
+  },
+];
+
+const teamNac = [
+  {
+    name: "Giancarlos Arauz",
+    role: "Coordinador de Comunicaciones",
+    image: "./src/assets/img/giancarlos.png",
+  },
+  {
+    name: "Blanca Agudo",
+    role: "Coordinador de Eventos",
+    image: "./src/assets/img/blanca.png",
+  },
+  {
+    name: "Jose Dominguez",
+    role: "Coordinador de Servicio Cristiano",
+    image: "./src/assets/img/jose.png",
+  },
+  {
+    name: "Jean Alabarca",
+    role: "Administrador",
+    image: "./src/assets/img/jean.png",
+  },
+];
+
+const cmDist = [
+  {
+    name: "Marcos Sanchez",
+    role: "Coordinador distrito A1",
+    image: "./src/assets/img/marcos.png",
+  },
+  {
+    name: "Edward Chiru",
+    role: "Coordinador distrito A3",
+    image: "./src/assets/img/edward.png",
+  },
+  {
+    name: "Edgar Tapia",
+    role: "Coordinador distrito A4",
+    image: "./src/assets/img/edgar.png",
+  },
+];
 
 const Org = () => {
   return (
     <div id="org" className="p-5 w-full mx-auto p-8 bg-white">
-      <div className="bg-gray-300 p-10 rounded-md">
-        <div className="text-center  text-gray-700 mt-4 mb-10">
+      <div className="bg-gray-300 p-2 rounded-md">
+        <div className="text-center text-gray-700 mt-4 mb-10">
           <h1 className="lg:text-5xl font-serif text-gray-800 text-3xl">
             Nuestra Organizacion Nacional
           </h1>
         </div>
 
         <div className="lg:flex justify-center">
-          <div className="flex bg-white border text-gray-700 border-gray-300 shadow-lg rounded lg:w-5/12 md:w-auto  p-1  items-center">
-            <div className=" w-24 h-24 shadow-lg rounded-full overflow-hidden">
-              <img
-                src="./src/assets/img/arturo.png"
-                className="object-cover w-full h-full"
-              />
+          {coorNac.map((person, index) => (
+            <div
+              key={index}
+              className="flex bg-white border text-gray-700 border-gray-300 shadow-lg rounded lg:w-5/12 md:w-auto p-1 items-center"
+            >
+              <div className="w-24 h-24 shadow-lg rounded-full overflow-hidden">
+                <img
+                  src={person.image}
+                  className="object-cover w-full h-full"
+                  alt={person.name}
+                />
+              </div>
+              <div className="ml-0 justify-center" style={{ flex: 2 }}>
+                <h3 className="text-xl font-semibold ">{person.name}</h3>
+                <p className="text-gray-600">{person.role}</p>
+              </div>
             </div>
-            <div className="ml-8 justify-center " style={{ flex: 2 }}>
-              <h3 className="text-xl font-semibold ">Arturo Martez</h3>
-              <p className="text-gray-600">Coordinador Nacional</p>
-            </div>
-          </div>
+          ))}
         </div>
 
         <br />
 
-        <div className="grid lg:grid-cols-3 md:grid-cols-2 gap-2 text-gray-700">
-          <div className="bg-white border border-gray-300 shadow-lg flex rounded w-auto pr-1 items-center">
-            <div className=" w-24 h-24 rounded-full shadow-lg overflow-hidden m-2">
-              <img
-                src="./src/assets/img/urbano.png"
-                className="object-cover w-full h-full "
-              />
+        <div className="lg:flex justify-center gap-1">
+          {comNac.map((person, index) => (
+            <div
+              key={index}
+              className="flex bg-white border text-gray-700 border-gray-300 shadow-lg rounded lg:w-5/12 md:w-auto p-1 items-center"
+            >
+              <div className="w-24 h-24 shadow-lg rounded-full overflow-hidden">
+                <img
+                  src={person.image}
+                  className="object-cover w-full h-full"
+                  alt={person.name}
+                />
+              </div>
+              <div className="ml-2 justify-center" style={{ flex: 2 }}>
+                <h3 className="text-xl font-semibold ">{person.name}</h3>
+                <p className="text-gray-600 text-sm">{person.role}</p>
+              </div>
             </div>
-            <div className="pl-1 " style={{ flex: 2 }}>
-              <h3 className="text-xl font-semibold ">Urbano Caceres</h3>
-              <p className="text-gray-600">Coordinador de Adiestramiento</p>
-            </div>
-          </div>
-          <div className="bg-white border border-gray-300 shadow-lg flex rounded w-auto pr-1 items-center">
-            <div className="w-24 h-24 rounded-full shadow-lg overflow-hidden m-2">
-              <img
-                src="./src/assets/img/luis.png"
-                className="object-cover w-full h-full"
-              />
-            </div>
-            <div className="pl-1 " style={{ flex: 2 }}>
-              <h3 className="text-xl font-semibold ">Luis Lopez</h3>
-              <p className="text-gray-600">Coordinador de Alcance</p>
-            </div>
-          </div>
-          <div className="bg-white border border-gray-300 shadow-lg flex rounded w-auto pr-1 items-center">
-            <div className="w-24 h-24 rounded-full shadow-lg overflow-hidden m-2">
-              <img
-                src="./src/assets/img/rafael.png"
-                className="object-cover w-full h-full"
-              />
-            </div>
-            <div className="pl-1 " style={{ flex: 2 }}>
-              <h3 className="text-xl font-semibold ">Rafael Romero</h3>
-              <p className="text-gray-600">Coordinador de Programa</p>
-            </div>
-          </div>
+          ))}
         </div>
 
         <br />
 
-        <div className="grid lg:grid-cols-4 md:grid-cols-2 gap-2 text-gray-700">
-          <div className="bg-white border border-gray-300 shadow-lg flex rounded w-auto pr-1 items-center">
-            <div className="w-24 h-24 rounded-full shadow-lg overflow-hidden m-2">
-              <img
-                src="./src/assets/img/giancarlos.png"
-                className="object-cover w-full h-full"
-              />
+        <div className=" lg:flex justify-center gap-1">
+          {teamNac.map((person, index) => (
+            <div
+              key={index}
+              className="flex bg-white border text-gray-700 border-gray-300 shadow-lg rounded lg:w-5/12 md:w-auto p-1 items-center"
+            >
+              <div className="w-24 h-24 shadow-lg rounded-full overflow-hidden">
+                <img
+                  src={person.image}
+                  className="object-cover w-full h-full"
+                  alt={person.name}
+                />
+              </div>
+              <div className="justify-center" style={{ flex: 2 }}>
+                <h3 className="text-xl font-semibold ">{person.name}</h3>
+                <p className="text-gray-600 text-sm">{person.role}</p>
+              </div>
             </div>
-            <div className="pl-1 " style={{ flex: 2 }}>
-              <h3 className="text-xl font-semibold ">Giancarlos Arauz</h3>
-              <p className="text-gray-600 text-xs">
-                Coordinador de Comunicaciones
-              </p>
-            </div>
-          </div>
-          <div className="bg-white border border-gray-300 shadow-lg flex rounded w-auto pr-1 items-center">
-            <div className="w-24 h-24 rounded-full shadow-lg overflow-hidden m-2">
-              <img
-                src="./src/assets/img/blanca.png"
-                className="object-cover w-full h-full"
-              />
-            </div>
-            <div className="pl-1 " style={{ flex: 2 }}>
-              <h3 className="text-xl font-semibold ">Blanca Agudo</h3>
-              <p className="text-gray-600 text-sm">Coordinador de Eventos</p>
-            </div>
-          </div>
-          <div className="bg-white border border-gray-300 shadow-lg flex rounded w-auto pr-1 items-center">
-            <div className="w-24 h-24 rounded-full shadow-lg overflow-hidden m-2">
-              <img
-                src="./src/assets/img/jose.png"
-                className="object-cover w-full h-full"
-              />
-            </div>
-            <div className="pl-1 " style={{ flex: 2 }}>
-              <h3 className="text-xl font-semibold ">Jose Dominguez</h3>
-              <p className="text-gray-600 text-xs ">
-                Coordinador de Servicio Cristiano
-              </p>
-            </div>
-          </div>
-          <div className="bg-white border border-gray-300 shadow-lg flex rounded w-auto pr-1 items-center">
-            <div className="w-24 h-24 rounded-full shadow-lg overflow-hidden m-2">
-              <img
-                src="./src/assets/img/jean.png"
-                className="object-cover w-full h-full"
-              />
-            </div>
-            <div className="pl-1 " style={{ flex: 2 }}>
-              <h3 className="text-xl font-semibold ">Jean Alabarca</h3>
-              <p className="text-gray-600">Administrador</p>
-            </div>
-          </div>
+          ))}
         </div>
 
         <br />
 
-        <div className="grid lg:grid-cols-3 md:grid-cols-2 gap-2 text-gray-700">
-          <div className="bg-white border border-gray-300 shadow-lg flex rounded w-auto pr-1 items-center">
-            <div className="w-24 h-24 rounded-full shadow-lg overflow-hidden m-2">
-              <img
-                src="./src/assets/img/marcos.png"
-                className="object-cover w-full h-full"
-              />
+        <div className=" lg:flex justify-center gap-1">
+          {cmDist.map((person, index) => (
+            <div
+              key={index}
+              className="flex bg-white border text-gray-700 border-gray-300 shadow-lg rounded lg:w-5/12 md:w-auto p-1 items-center"
+            >
+              <div className="w-24 h-24 shadow-lg rounded-full overflow-hidden">
+                <img
+                  src={person.image}
+                  className="object-cover w-full h-full"
+                  alt={person.name}
+                />
+              </div>
+              <div className="justify-center" style={{ flex: 2 }}>
+                <h3 className="text-xl font-semibold ">{person.name}</h3>
+                <p className="text-gray-600 text-sm">{person.role}</p>
+              </div>
             </div>
-            <div className="pl-1 " style={{ flex: 2 }}>
-              <h3 className="text-xl font-semibold ">Marcos Sanchez</h3>
-              <p className="text-gray-600 ">Coordinador distrito A1</p>
-            </div>
-          </div>
-          <div className="bg-white border border-gray-300 shadow-lg flex rounded w-auto pr-1 items-center">
-            <div className="w-24 h-24 rounded-full shadow-lg overflow-hidden m-2">
-              <img
-                src="./src/assets/img/edward.png"
-                className="object-cover w-full h-full"
-              />
-            </div>
-            <div className="pl-1 " style={{ flex: 2 }}>
-              <h3 className="text-xl font-semibold ">Edward Chiru</h3>
-              <p className="text-gray-600">Coordinador distrito A3</p>
-            </div>
-          </div>
-          <div className="bg-white border border-gray-300 shadow-lg flex rounded w-auto pr-1 items-center">
-            <div className="w-24 h-24 rounded-full shadow-lg overflow-hidden m-2">
-              <img
-                src="./src/assets/img/edgar.png"
-                className="object-cover w-full h-full"
-              />
-            </div>
-            <div className="pl-1 " style={{ flex: 2 }}>
-              <h3 className="text-xl font-semibold ">Edgar Tapia</h3>
-              <p className="text-gray-600 ">Coordinador distrito A4</p>
-            </div>
-          </div>
+          ))}
         </div>
+
       </div>
     </div>
   );
