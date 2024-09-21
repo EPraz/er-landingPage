ed95866698269dcaf626f2518b7ae1b4e79a6b08
unknown <epilepra@gmail.com>
2024-09-21 18:34:08 -0500

    amend main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,17 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import { createHashRouter, RouterProvider } from "react-router-dom";
-
-const router = createHashRouter([
-  {
-    path: "/*",
-    element: <App />,
-  },
-]);
+import { HashRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <HashRouter>
+      <App />
+    </HashRouter>
   </React.StrictMode>
 );
