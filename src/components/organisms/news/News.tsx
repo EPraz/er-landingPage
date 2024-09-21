43bec047a08ca04f37d9c0908fa4c1d596761fc1
unknown <epilepra@gmail.com>
2024-09-21 18:50:54 -0500

    amend News.tsx:

--- src/components/organisms/news/News.tsx
+++ src/components/organisms/news/News.tsx
@@ -2,88 +2,70 @@ import { useState, useEffect } from "react";
 import { BsChevronCompactLeft, BsChevronCompactRight } from "react-icons/bs";
 
 function Slider() {
-    const slides = [{
-            url: "./src/assets/img/s-1.jpg",
-        },
-        {
-            url: "./src/assets/img/s-2.jpg",
-        },
-        {
-            url: "./src/assets/img/s-3.png",
-        },
-    ];
+  const slides = [
+    {
+      url: "./src/assets/img/s-1.jpg",
+    },
+    {
+      url: "./src/assets/img/s-2.jpg",
+    },
+    {
+      url: "./src/assets/img/s-3.png",
+    },
+  ];
 
-    const [currentIndex, setCurrentIndex] = useState(0);
+  const [currentIndex, setCurrentIndex] = useState(0);
 
+  const nextSlide = () => {
+    const newIndex = (currentIndex + 1) % slides.length;
+    setCurrentIndex(newIndex);
+  };
 
-    const nextSlide = () => {
-        const newIndex = (currentIndex + 1) % slides.length;
-        setCurrentIndex(newIndex);
-    };
+  const prevSlide = () => {
+    const newIndex = currentIndex === 0 ? slides.length - 1 : currentIndex - 1;
+    setCurrentIndex(newIndex);
+  };
 
+  const goToSlide = (slideIndex) => {
+    setCurrentIndex(slideIndex);
+  };
 
-    const prevSlide = () => {
-        const newIndex = currentIndex === 0 ? slides.length - 1 : currentIndex - 1;
-        setCurrentIndex(newIndex);
-    };
+  useEffect(() => {
+    const interval = setInterval(() => {
+      nextSlide();
+    }, 5000);
 
+    return () => clearInterval(interval);
+  }, [currentIndex]);
 
-    const goToSlide = (slideIndex) => {
-        setCurrentIndex(slideIndex);
-    };
-
-
-    useEffect(() => {
-        const interval = setInterval(() => {
-            nextSlide();
-        }, 5000);
-
-
-        return () => clearInterval(interval);
-    }, [currentIndex]);
-
-    return ( <
-        div className = "w-full mx-auto bg-white p-4" >
-        <
-        div className = "h-[500px] w-full m-auto py-1 px-1 relative group" >
-        <
-        div style = {
-            { backgroundImage: `url(${slides[currentIndex].url})` } }
-        className = "w-full h-full rounded-md bg-center bg-cover duration-500" >
-        < /div>
-
-        <
-        div className = "hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] left-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer" >
-        <
-        BsChevronCompactLeft onClick = { prevSlide }
-        size = { 30 }
-        /> <
-        /div>
-
-        <
-        div className = "hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] right-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer" >
-        <
-        BsChevronCompactRight onClick = { nextSlide }
-        size = { 30 }
-        /> <
-        /div> <
-        div className = "flex top-4 justify-center py-2" > {
-            slides.map((slide, slideIndex) => ( <
-                div key = { slideIndex }
-                onClick = {
-                    () => goToSlide(slideIndex) }
-                className = { `text-2xl cursor-pointer ${
+  return (
+    <div className="w-full mx-auto bg-white p-4">
+      <div className="h-[500px] w-full m-auto py-1 px-1 relative group">
+        <div
+          style={{ backgroundImage: `url(${slides[currentIndex].url})` }}
+          className="w-full h-full rounded-md bg-center bg-cover duration-500"
+        ></div>
+        <div className="hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] left-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer">
+          <BsChevronCompactLeft onClick={prevSlide} size={30} />
+        </div>
+        <div className="hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] right-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer">
+          <BsChevronCompactRight onClick={nextSlide} size={30} />{" "}
+        </div>{" "}
+        <div className="flex top-4 justify-center py-2">
+          {" "}
+          {slides.map((slide, slideIndex) => (
+            <div
+              key={slideIndex}
+              onClick={() => goToSlide(slideIndex)}
+              className={`text-2xl cursor-pointer ${
                 slideIndex === currentIndex ? "text-blue-500" : ""
-              }` } >
-
-                <
-                /div>
-            ))
-        } <
-        /div> <
-        /div> <
-        /div>
-    );
+              }`}
+            ></div>
+          ))}{" "}
+        </div>{" "}
+      </div>{" "}
+    </div>
+  );
 }
 
 export default Slider;
